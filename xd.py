243c573cd96b963fea32a01e45b1075c4521c08a
VADOSXD <vadimkz282@gmail.com>
2026-02-23 01:52:19 +0300

    defы

--- xd.py
+++ xd.py
@@ -1,26 +1,26 @@
 from random import uniform
 from tkinter import *
 
-newsens = f'  "MouseSensitivity" : {round(uniform(0.1,0.6),2)}, \n'
-
-#def TarkovSens(CFGPath):
-    #print(round(uniform(0.1, 0.5),2))
-with open("Control.ini", "r") as file:
-    filedata = file.read()
-
-with open("Control.ini", "r") as file:        
-    for line in file:
-        if "MouseSensitivity" in line:
-            oldsens = line
 
 
-print(oldsens)
-filedata = filedata.replace(oldsens, newsens)
+def TarkovSens(CFGPath):
+    newsens = f'  "MouseSensitivity" : {round(uniform(0.1,0.6),2)}, \n'
+    with open(f"{CFGPath}", "r") as file:
+        filedata = file.read()
 
-with open("Control.ini" , "w") as file:
-    file.write(filedata)
+    with open(f"{CFGPath}", "r") as file:        
+        for line in file:
+            if "MouseSensitivity" in line:
+                oldsens = line
 
 
+    print(oldsens)
+    filedata = filedata.replace(oldsens, newsens)
+
+    with open(f"{CFGPath}" , "w") as file:
+        file.write(filedata)
+
+TarkovSens("/Users/kuzmenkovvadim/Documents/sensrandomizer/Control.ini")
 
 
 
