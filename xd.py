742d581c82f9a36f3ca423be8101c72501293a2e
VADOSXD <vadimkz282@gmail.com>
2026-02-23 11:12:31 +0300

    разделение аимсенсы и обычной сенсы

--- xd.py
+++ xd.py
@@ -4,18 +4,23 @@ from tkinter import *
 
 
 def TarkovSens(CFGPath):
-    newsens = f'  "MouseSensitivity" : {round(uniform(0.1,0.6),2)}, \n'
+    SensDigit = round(uniform(0.1, 0.6), 2)
+    newsens = f'  "MouseSensitivity" : {SensDigit}, \n'
+    aimsens = f'  "MouseAimingSensitivity" : {SensDigit}, \n'
     with open(f"{CFGPath}", "r") as file:
         filedata = file.read()
 
     with open(f"{CFGPath}", "r") as file:        
         for line in file:
+            if "MouseAimingSensitivity" in line:
+                oldaimsens = line 
             if "MouseSensitivity" in line:
                 oldsens = line
 
 
     print(oldsens)
     filedata = filedata.replace(oldsens, newsens)
+    filedata = filedata.replace(oldaimsens, aimsens)
 
     with open(f"{CFGPath}" , "w") as file:
         file.write(filedata)
